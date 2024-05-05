2f555a828853cb83bfaba459f9d732d3dde2c3fc
soaemluhana <kumarsoaem@gmail.com>
2024-05-05 16:32:54 +0500

    VART

---
+++ app/src/main/java/com/example/vart/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.vart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+    TextView signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.etUsername);
+        password = findViewById(R.id.etPassword);
+        login = findViewById(R.id.btnLogin);
+        signup = findViewById(R.id.tvSignup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
