c17fc6728738b7fc15c79f4550e5f1cb184da5a8
AbdulMoiz132 <moizmemon457@gmail.com>
2024-05-21 03:42:39 +0500

    Search bar added

--- app/src/main/java/com/example/vart/ArtistAdapter.java
+++ app/src/main/java/com/example/vart/ArtistAdapter.java
@@ -3,50 +3,62 @@ package com.example.vart;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistViewHolder> {
 
-    private ArrayList<String> artistList;
-    private OnItemClickListener listener;
+    private List<TopArtist> artists = new ArrayList<>();
 
-    public interface OnItemClickListener {
-        void onItemClick(String artistName);
-    }
-    public ArtistAdapter(ArrayList<String> artistList, OnItemClickListener listener) {
-        this.artistList = artistList;
-        this.listener = listener;
+    public void setArtists(List<TopArtist> artists) {
+        this.artists = artists;
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public ArtistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.followed, parent, false);
         return new ArtistViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ArtistViewHolder holder, int position) {
-        holder.textView.setText(artistList.get(position));
-
+        TopArtist artist = artists.get(position);
+        // Bind artist data to the view
+        holder.bind(artist);
     }
 
     @Override
     public int getItemCount() {
-        return artistList.size();
+        return artists.size();
     }
 
-    static class ArtistViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
+    public static class ArtistViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView usernameTextView;
+        private ImageView profileImageView;
 
         public ArtistViewHolder(@NonNull View itemView) {
             super(itemView);
-            textView = itemView.findViewById(android.R.id.text1);
+            usernameTextView = itemView.findViewById(R.id.user);
+            profileImageView = itemView.findViewById(R.id.profile);
+        }
+
+        public void bind(TopArtist artist) {
+            // Bind artist data to views
+            usernameTextView.setText(artist.getusername());
+            // Load artist profile image using Glide or Picasso
+            Picasso.get().load(artist.getImage()).placeholder(R.drawable.default_profile).into(profileImageView);
         }
     }
 }
-
