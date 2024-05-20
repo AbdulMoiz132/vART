ccfa20ca01096cd831e8ac2be10bbaffdf540531
AbdulMoiz132 <moizmemon457@gmail.com>
2024-05-20 16:12:57 +0500

    Liked page and followers update

--- app/src/main/java/com/example/vart/HomeActivity.java
+++ app/src/main/java/com/example/vart/HomeActivity.java
@@ -182,4 +182,6 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 });
     }
+
 }
+
--- app/src/main/java/com/example/vart/LikedFragment.java
+++ app/src/main/java/com/example/vart/LikedFragment.java
@@ -19,6 +19,7 @@ import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.firestore.CollectionReference;
@@ -43,65 +44,58 @@ public class LikedFragment extends Fragment implements LikedAdapter.OnLikedArtCl
 
     private String username;
 
-
+    private TextView likedText;
 
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_liked, container, false);
 
+        db = FirebaseFirestore.getInstance();
         recyclerView = rootview.findViewById(R.id.likedRecyclerView);
 
         likedArts = new ArrayList<>();
+
+        likedText = rootview.findViewById(R.id.tvNoLikedArts);
+        likedText.setVisibility(View.GONE);
 
         if (getArguments() != null) {
             username = getArguments().getString("username");
         }
 
 
-//        db.collection("liked").whereEqualTo("username", username)
-//                .get()
-//                .addOnCompleteListener(task -> {
-//                    if (task.isSuccessful()) {
-//                        for (QueryDocumentSnapshot document : task.getResult()) {
-//                            // Extract the data for each art piece
-//                            String artistUsername = document.getString("artistUsername");
-//                            String title = document.getString("title");
-//                            String imageurl = document.getString("imageurl");
-//                            LikedArts art = new LikedArts(imageurl, title, artistUsername);
-//                            likedArts.add(art);
-//                        }
-//                        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
-//                        adapter = new LikedAdapter(likedArts, this);
-//                        recyclerView.setAdapter(adapter);
-//                    } else
-//                    {
-//                        // Handle errors
-//                    }
-//                });
-
-        likedArts.add(new LikedArts("https://firebasestorage.googleapis.com/v0/b/vart-2329a.appspot.com/o/images%2Fart1.jpg?alt=media&token=34019c49-02cb-46ad-8b77-4d1945910640", "Murshid", "Ho"));
-        likedArts.add(new LikedArts("https://firebasestorage.googleapis.com/v0/b/vart-2329a.appspot.com/o/images%2Fart2.jpg?alt=media&token=ece0b9c7-2ed5-4f51-8c7c-29acba8f9b06","Warrior", "Hi"));
-        likedArts.add(new LikedArts("https://firebasestorage.googleapis.com/v0/b/vart-2329a.appspot.com/o/images%2F2024_05_16_13_18_02?alt=media&token=0c6626ac-c0fe-46a0-9ce0-6c96fa08b651","News", "Bye"));
-
-
-
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
-        adapter = new LikedAdapter(likedArts, this);
-        recyclerView.setAdapter(adapter);
+        db.collection("liked").whereEqualTo("user", username)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Extract the data for each art piece
+                            String artistUsername = document.getString("artist");
+                            String title = document.getString("title");
+                            String imageurl = document.getString("imageUrl");
+                            LikedArts art = new LikedArts(imageurl, title, artistUsername);
+                            likedArts.add(art);
+                        }
+                        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
+                        adapter = new LikedAdapter(likedArts, this);
+                        recyclerView.setAdapter(adapter);
+                    } else
+                    {
+                        likedText.setVisibility(View.VISIBLE);
+                    }
+                });
 
         return rootview;
     }
 
     public void onLikedArtClick(LikedArts art)
     {
-//        Intent intent = new Intent(getContext(),Post.class);
-//        intent.putExtra("username", username);
-//        intent.putExtra("artistUsername", art.getUsername());
-//        intent.putExtra("title", art.getTitle());
-//        intent.putExtra("artUrl", art.getImage());
-//        startActivity(intent);
-        Toast.makeText(getContext(), art.getTitle(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getContext(),Post.class);
+        intent.putExtra("username", username);
+        intent.putExtra("artistUsername", art.getUsername());
+        intent.putExtra("title", art.getTitle());
+        intent.putExtra("artUrl", art.getImage());
+        startActivity(intent);
     }
 }
 
