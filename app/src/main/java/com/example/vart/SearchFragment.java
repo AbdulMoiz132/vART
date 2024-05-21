37d296dc4fffd91e3e4e24a038cd78f1a955af71
soaemluhana <kumarsoaem@gmail.com>
2024-05-21 12:57:36 +0500

    for moiz to work on search fragment

---
+++ app/src/main/java/com/example/vart/SearchFragment.java
@@ -0,0 +1,19 @@
+package com.example.vart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class SearchFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_search, container, false);
+
+        return rootview;
+    }
+
+}
